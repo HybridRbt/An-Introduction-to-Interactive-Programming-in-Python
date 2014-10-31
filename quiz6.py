c12d2b8d1efe95f564543572cb586df3c6449d9a
HybridRbt <littlejack0120@gmail.com>
2014-10-31 15:20:14 -0500

    test code for the quiz

--- quiz6.py
+++ quiz6.py
@@ -15,10 +15,10 @@ __author__ = 'jeredyang'
 
 
 # class Overload:
-#     def __init__(self, a_parameter):
-#         pass
+# def __init__(self, a_parameter):
+# pass
 #
-#     def __init__(self, a_parameter, b_parameter):
+# def __init__(self, a_parameter, b_parameter):
 #         pass
 #
 # a = Overload(0)
@@ -56,6 +56,7 @@ class BankAccount:
     def get_fees(self):
         """Returns the total fees ever deducted from the account."""
         return self.fee
+
 
 #
 # my_account = BankAccount(10)
@@ -116,58 +117,155 @@ class BankAccount:
 # account1.deposit(20)
 # account2.withdraw(20)
 # print account1.get_balance(), account1.get_fees(), account2.get_balance(), account2.get_fees()
-account1 = BankAccount(20)
-account1.deposit(10)
-account2 = BankAccount(10)
-account2.deposit(10)
-account2.withdraw(50)
-account1.withdraw(15)
-account1.withdraw(10)
-account2.deposit(30)
-account2.withdraw(15)
-account1.deposit(5)
-account1.withdraw(10)
-account2.withdraw(10)
-account2.deposit(25)
-account2.withdraw(15)
-account1.deposit(10)
-account1.withdraw(50)
-account2.deposit(25)
-account2.deposit(25)
-account1.deposit(30)
-account2.deposit(10)
-account1.withdraw(15)
-account2.withdraw(10)
-account1.withdraw(10)
-account2.deposit(15)
-account2.deposit(10)
-account2.withdraw(15)
-account1.deposit(15)
-account1.withdraw(20)
-account2.withdraw(10)
-account2.deposit(5)
-account2.withdraw(10)
-account1.deposit(10)
-account1.deposit(20)
-account2.withdraw(10)
-account2.deposit(5)
-account1.withdraw(15)
-account1.withdraw(20)
-account1.deposit(5)
-account2.deposit(10)
-account2.deposit(15)
-account2.deposit(20)
-account1.withdraw(15)
-account2.deposit(10)
-account1.deposit(25)
-account1.deposit(15)
-account1.deposit(10)
-account1.withdraw(10)
-account1.deposit(10)
-account2.deposit(20)
-account2.withdraw(15)
-account1.withdraw(20)
-account1.deposit(5)
-account1.deposit(10)
-account2.withdraw(20)
-print account1.get_balance(), account1.get_fees(), account2.get_balance(), account2.get_fees()
+# account1 = BankAccount(20)
+# account1.deposit(10)
+# account2 = BankAccount(10)
+# account2.deposit(10)
+# account2.withdraw(50)
+# account1.withdraw(15)
+# account1.withdraw(10)
+# account2.deposit(30)
+# account2.withdraw(15)
+# account1.deposit(5)
+# account1.withdraw(10)
+# account2.withdraw(10)
+# account2.deposit(25)
+# account2.withdraw(15)
+# account1.deposit(10)
+# account1.withdraw(50)
+# account2.deposit(25)
+# account2.deposit(25)
+# account1.deposit(30)
+# account2.deposit(10)
+# account1.withdraw(15)
+# account2.withdraw(10)
+# account1.withdraw(10)
+# account2.deposit(15)
+# account2.deposit(10)
+# account2.withdraw(15)
+# account1.deposit(15)
+# account1.withdraw(20)
+# account2.withdraw(10)
+# account2.deposit(5)
+# account2.withdraw(10)
+# account1.deposit(10)
+# account1.deposit(20)
+# account2.withdraw(10)
+# account2.deposit(5)
+# account1.withdraw(15)
+# account1.withdraw(20)
+# account1.deposit(5)
+# account2.deposit(10)
+# account2.deposit(15)
+# account2.deposit(20)
+# account1.withdraw(15)
+# account2.deposit(10)
+# account1.deposit(25)
+# account1.deposit(15)
+# account1.deposit(10)
+# account1.withdraw(10)
+# account1.deposit(10)
+# account2.deposit(20)
+# account2.withdraw(15)
+# account1.withdraw(20)
+# account1.deposit(5)
+# account1.deposit(10)
+# account2.withdraw(20)
+# print account1.get_balance(), account1.get_fees(), account2.get_balance(), account2.get_fees()
+
+# quiz 6b
+# Q5
+# def list_extend_many(lists):
+#     result = []
+#     i = 0
+#     while i < len(lists):
+#         result.extend(lists[i])
+#         i += 1
+#     return result
+# def list_extend_many(lists):
+#     result = []
+#     i = 0
+#     while i < len(lists):
+#         result += lists[i]
+#         i += 1
+#     return result
+#
+# def list_extend_many(lists):
+#     result = []
+#     i = 0
+#     while i < len(lists):
+#         result += lists[i]
+#         i += 1
+#     return result
+def list_extend_many(lists):
+    result = []
+    i = 0
+    while i < len(lists):
+        result.extend(lists[i])
+        i += 1
+    return result
+print list_extend_many([[1, 2], [3], [4, 5, 6], [7]])
+
+# Q7
+# n = 1000
+# numbers = range(2, n)
+#
+# result = []
+#
+#
+# def remove_number(a_list):
+#     divider = a_list[0]
+#     remove = []
+#     i = 0
+#     while i < len(a_list):
+#         if a_list[i] % divider == 0:
+#             remove.append(a_list[i])
+#         i += 1
+#
+#     for each_number in remove:
+#         a_list.remove(each_number)
+#
+#
+# while len(numbers) > 0:
+#     result.append(numbers[0])
+#     remove_number(numbers)
+#
+# print len(result)
+
+#Q8
+# slow_wumpuses = 1000
+# Fast_Wumpuses = 1
+#
+#
+# def num_of_slow(year):
+#     global slow_wumpuses
+#
+#     if year == 1:
+#         num = slow_wumpuses
+#     else:
+#         num = 2 * num_of_slow(year - 1) * 0.6
+#
+#     return num
+#
+#
+# def num_of_fast(year):
+#     global Fast_Wumpuses
+#
+#     if year == 1:
+#         num = Fast_Wumpuses
+#     else:
+#         num = 2 * num_of_fast(year - 1) * 0.7
+#
+#     return num
+#
+#
+# def out_number():
+#     y = 1
+#     while num_of_slow(y) > num_of_fast(y):
+#         y += 1
+#         if y > 43:
+#             print y, num_of_slow(y), num_of_fast(y)
+#
+#     return y
+#
+# print out_number()
